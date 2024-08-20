30552c11ff36a7467c3954dabb2a1ef72b11bcdd
Somarasu <mhaynes1337@gmail.com>
2024-08-19 22:39:18 -0700

    new version of portfolio site

--- wu-test/src/js/favorites/favorites.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.navbar-burger').addEventListener('click', function(){
-	document.querySelector('.navbar-menu').classList.toggle('is-active')
-	this.classList.toggle("is-active") 
-});
